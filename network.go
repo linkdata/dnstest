0c8b836915c85a06b6ad06d081eabfa060e9f331
Johan Lindh <johan@linkdata.se>
2025-09-23 08:54:05 +0200

    wip

--- network.go
+++ network.go
@@ -1,32 +1,40 @@
 package dnstest
 
 import (
+	"context"
+	"net"
+
 	"github.com/miekg/dns"
+	"golang.org/x/net/proxy"
 )
+
+var _ proxy.ContextDialer = &Network{}
 
 type Network struct {
 }
 
-func NewNetwork(msgs []*dns.Msg) (nw *Network, err error) {
+func NewNetwork(exchs []Exchange) (nw *Network, err error) {
 	aSet := map[string]map[string]struct{}{} // A record qname -> set of IPs
 	nsSet := map[string]struct{}{}           // NS records
-	for _, msg := range msgs {
-		if msg != nil {
-			for _, rrs := range [][]dns.RR{msg.Answer, msg.Ns, msg.Answer} {
-				for _, rr := range rrs {
-					qname := dns.CanonicalName(rr.Header().Name)
-					switch rr := rr.(type) {
-					case *dns.A:
-						aSet[qname][rr.A.String()] = struct{}{}
-					case *dns.AAAA:
-						aSet[qname][rr.AAAA.String()] = struct{}{}
-					case *dns.NS:
-						nsSet[dns.CanonicalName(rr.Ns)] = struct{}{}
-					}
+	for _, exch := range exchs {
+		for _, rrs := range [][]dns.RR{exch.Answer, exch.Ns, exch.Answer} {
+			for _, rr := range rrs {
+				qname := dns.CanonicalName(rr.Header().Name)
+				switch rr := rr.(type) {
+				case *dns.A:
+					aSet[qname][rr.A.String()] = struct{}{}
+				case *dns.AAAA:
+					aSet[qname][rr.AAAA.String()] = struct{}{}
+				case *dns.NS:
+					nsSet[dns.CanonicalName(rr.Ns)] = struct{}{}
 				}
 			}
 		}
 	}
 
+	return
+}
+
+func (n *Network) DialContext(ctx context.Context, network string, address string) (conn net.Conn, err error) {
 	return
 }
